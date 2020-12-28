a6e8415894e8cb14ad87d362010244826752f663
Ulas Erdogan <uerdogan2001@hotmail.com>
2020-12-29 01:18:58 +0300

    list booking function

--- main.c
+++ main.c
@@ -10,9 +10,9 @@ Serdar Yaşar - 010190077
 #include <conio.h>  // included this library to take getch() function to keep console window opened at the end of the program
 #include <string.h> // included this library to use string functions
 
-#define AIRPORT_NAME_LENGTH 20 // the maximum length of the airport names' strings are being controlled by "AIRPORT_NAME_LENGTH" macro
-#define PASSWORD_LENGTH 20     // the maximum length of the adminstrator passwords is being controlled by "PASSWORD_LENGTH" macro
-#define NAME_LENGTH 30         // the maximum length of the name is being controlled by "NAME_LENGTH" macro
+#define AIR_NAME_LENGTH 20 // the maximum length of the airport and airlines names' strings are being controlled by "AIR_NAME_LENGTH" macro
+#define PASSWORD_LENGTH 20 // the maximum length of the adminstrator passwords is being controlled by "PASSWORD_LENGTH" macro
+#define NAME_LENGTH 30     // the maximum length of the name is being controlled by "NAME_LENGTH" macro
 
 enum boolean
 {
@@ -30,12 +30,14 @@ void createPassword();
 int showMainMenu();
 // menu switches designing function
 void design();
-// function 
-int showAdminMenu();
+// function that provides log in by password check
+int adminLogin();
 // function that shows the admin menu
 int showAdminMenu();
 // function that records flights to file database
-void addFlight();
+int addFlight();
+// function that lists and prints available flights
+int listFlights();
 
 // structs that we used in our program
 
@@ -43,12 +45,13 @@ void addFlight();
 typedef struct _Flight
 {
     // "flight code, departure airport, destination airport, time of departure, time of destination and passenger capacity" records
-    int flightCode;                        // between 1111 - 9999
-    char depAirport[AIRPORT_NAME_LENGTH];  // the maximum length of the airport names' strings are being controlled by "AIRPORT_NAME_LENGTH" macro
-    char destAirport[AIRPORT_NAME_LENGTH]; // the maximum length of the airport names' strings are being controlled by "AIRPORT_NAME_LENGTH" macro
-    float timeOfDep;                     // "HH.MM" format : time of departure
-    float timeOfDest;                    // "HH.MM" format : time of destination
-    int passengerCapacity;                 // passenger capacities of the planes
+    int flightCode;
+    char airlines[AIR_NAME_LENGTH];    // between 1111 - 9999
+    char depAirport[AIR_NAME_LENGTH];  // the maximum length of the airport names' strings are being controlled by "AIR_NAME_LENGTH" macro
+    char destAirport[AIR_NAME_LENGTH]; // the maximum length of the airport names' strings are being controlled by "AIR_NAME_LENGTH" macro
+    float timeOfDep;                   // "HH.MM" format : time of departure
+    float timeOfDest;                  // "HH.MM" format : time of destination
+    int passengerCapacity;             // passenger capacities of the planes
 } Flight;
 
 // a structuro of the bookings that passengers recorded
@@ -93,6 +96,7 @@ int main()
                     break;
                 // listing current flights
                 case 4:
+                    listFlights();
                     break;
                 // listing bookings
                 case 5:
@@ -108,7 +112,7 @@ int main()
                 default:
                     printf("\n!!!!! An error occurred");
                 }
-                break;
+
             // passenger menu option
             case 2:
                 printf("passenger\n");
@@ -165,7 +169,7 @@ void createPassword()
     //if there are not a password record, create password and provide sign up; else verify the old password and change it
     if (fopen("password.txt", "r") == NULL)
     {
-        design();
+        design(); // designing
         printf("Please enter your new password (less than 20 characters): ");
         scanf("%s", password);
         pwPtr = fopen("password.txt", "w");
@@ -174,7 +178,7 @@ void createPassword()
     // section to verify old password and change it
     else
     {
-        design();
+        design();                             // designing
         char oldPassword[3][PASSWORD_LENGTH]; // a string array to verify be used in verifing passwords
         printf("Please, verify your password: ");
         scanf("%s", oldPassword[1]); // 1 indexed element is for checking old password
@@ -186,7 +190,7 @@ void createPassword()
 
         if (!strcmp(oldPassword[1], oldPassword[2]) && !strcmp(oldPassword[0], oldPassword[1])) // && !strcmp(oldPassword[0], DOSYADAKİ ŞİFRE) FILE OKUYUP KARŞILAŞTIRMA ÖZELLİĞİ EKLENECEK
         {
-            design();
+            design(); // designing
             printf("Please enter your new password (less than 20 characters): ");
             scanf("%s", password);
             pwPtr = fopen("password.txt", "w");
@@ -194,7 +198,7 @@ void createPassword()
         }
         else
         {
-            design();
+            design(); // designing
             printf("The passwords that you entered didn't match...");
         }
     }
@@ -204,7 +208,7 @@ void createPassword()
 // a function to show the main menu
 int showMainMenu()
 {
-    design();
+    design(); // designing
 
     int choice; // variable that will be returned as choice
     printf("1. Enter as adminstrator\n");
@@ -219,7 +223,7 @@ int showMainMenu()
 // a function to provide access to the admin menu by password checking
 int adminLogin()
 {
-    design();
+    design(); // designing
 
     FILE *pwPtr;                        // pointer for password file database
     char password[2][PASSWORD_LENGTH];  // a string array that keeps passwords entered and signupped to match to login
@@ -245,24 +249,23 @@ int adminLogin()
 // a function to open adminstrator menu if the admin has loginned succesfully
 int showAdminMenu()
 {
-        int choice;
-        printf("1. Add a new flight\n");
-        printf("2. Edit available flights\n");
-        printf("3. Delete a flight\n");
-        printf("4. List all available flights\n");
-        printf("5. List all bookings\n");
-        printf("6. Change your password\n");
-        printf("0. Return to the main menu\n");
-        printf("\nPlease make your menu option: ");
-        scanf("%d", &choice);
-        return choice;
-
+    int choice;
+    printf("1. Add a new flight\n");
+    printf("2. Edit available flights\n");
+    printf("3. Delete a flight\n");
+    printf("4. List all available flights\n");
+    printf("5. List all bookings\n");
+    printf("6. Change your password\n");
+    printf("0. Return to the main menu\n");
+    printf("\nPlease make your menu option: ");
+    scanf("%d", &choice);
+    return choice;
 }
 
 // a function to add flights by adminstrator to file database
-void addFlight()
+int addFlight()
 {
-    design();
+    design(); // designing
 
     Flight newFlight; // creation of flight struct to make records
     Flight *nfPtr;    // pointer to manage struct
@@ -279,6 +282,8 @@ void addFlight()
 
     // inputting required informations
     nfPtr->flightCode = 1111;
+    printf("Please enter the airlines: ");
+    scanf("%s", nfPtr->airlines);
     printf("Please enter the departure airport: ");
     scanf("%s", nfPtr->depAirport);
     printf("\nPlease enter the destination airport: ");
@@ -292,11 +297,52 @@ void addFlight()
 
     // open the file, handle the inputted information and close
     flPtr = fopen("flights.txt", "a");
-    fprintf(flPtr, "%d %s %s %.2f %.2f %d\n", nfPtr->flightCode, nfPtr->depAirport, nfPtr->destAirport, nfPtr->timeOfDep, nfPtr->timeOfDest, nfPtr->passengerCapacity);
+    fprintf(flPtr, "%d %s %s %s %.2f %.2f %d\n", nfPtr->flightCode, nfPtr->airlines, nfPtr->depAirport, nfPtr->destAirport, nfPtr->timeOfDep, nfPtr->timeOfDest, nfPtr->passengerCapacity);
     printf("\n The flight has recorded succesfully...\n");
     fclose(flPtr);
 
     //return to the admin menu after the process
-    design();
-    showAdminMenu();
+    design(); // designing
+    return showAdminMenu();
+}
+
+// a function to list added flights from the file database
+int listFlights()
+{
+    design(); // designing
+
+    FILE *flPtr;                       // pointer for flights file database
+    flPtr = fopen("flights.txt", "r"); // opening file database to read lines
+
+    Flight readFlight; // struct for placing the flight informations that have read
+    Flight *rfPtr;     // pointer to manage struct
+    rfPtr = &readFlight;
+
+    // reading the first records on the data
+    fscanf(flPtr, "%d", &rfPtr->flightCode);
+    fscanf(flPtr, "%s", rfPtr->airlines);
+    fscanf(flPtr, "%s", rfPtr->depAirport);
+    fscanf(flPtr, "%s", rfPtr->destAirport);
+    fscanf(flPtr, "%f", &rfPtr->timeOfDep);
+    fscanf(flPtr, "%f", &rfPtr->timeOfDest);
+    fscanf(flPtr, "%d", &rfPtr->passengerCapacity);
+
+    // keep reading data from the file while not end of the file and print them
+    while (!feof(flPtr))
+    {
+        printf("%d %s %s %s %.2f %.2f %d\n", rfPtr->flightCode, rfPtr->airlines, rfPtr->depAirport, rfPtr->destAirport, rfPtr->timeOfDep, rfPtr->timeOfDest, rfPtr->passengerCapacity);
+        fscanf(flPtr, "%d", &rfPtr->flightCode);
+        fscanf(flPtr, "%s", rfPtr->airlines);
+        fscanf(flPtr, "%s", rfPtr->depAirport);
+        fscanf(flPtr, "%s", rfPtr->destAirport);
+        fscanf(flPtr, "%f", &rfPtr->timeOfDep);
+        fscanf(flPtr, "%f", &rfPtr->timeOfDest);
+        fscanf(flPtr, "%d", &rfPtr->passengerCapacity);
+    }
+
+    fclose(flPtr); // close the file
+
+    //return to the admin menu after the process
+    design(); // designing
+    return showAdminMenu();
 }
