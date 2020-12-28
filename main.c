bfe97cab0b897654484e484c90ff3cf2937385d7
Ulas Erdogan <uerdogan2001@hotmail.com>
2020-12-28 22:19:34 +0300

    login function

--- main.c
+++ main.c
@@ -7,13 +7,12 @@ Serdar Yaşar - 010190077
 */
 
 #include <stdio.h>
-#include <conio.h> // included this library to take getch() function to keep console window opened at the end of the program
+#include <conio.h>  // included this library to take getch() function to keep console window opened at the end of the program
 #include <string.h> // included this library to use string functions
-#include"design.h"
 
 #define AIRPORT_NAME_LENGTH 20 // the maximum length of the airport names' strings are being controlled by "AIRPORT_NAME_LENGTH" macro
-#define PASSWORD_LENGTH 20 // the maximum length of the adminstrator passwords is being controlled by "PASSWORD_LENGTH" macro
-#define NAME_LENGTH 30 // the maximum length of the name is being controlled by "NAME_LENGTH" macro
+#define PASSWORD_LENGTH 20     // the maximum length of the adminstrator passwords is being controlled by "PASSWORD_LENGTH" macro
+#define NAME_LENGTH 30         // the maximum length of the name is being controlled by "NAME_LENGTH" macro
 
 enum boolean
 {
@@ -31,8 +30,12 @@ void createPassword();
 int showMainMenu();
 // menu switches designing function
 void design();
+// function 
+int showAdminMenu();
 // function that shows the admin menu
 int showAdminMenu();
+// function that records flights to file database
+void addFlight();
 
 // structs that we used in our program
 
@@ -40,22 +43,22 @@ int showAdminMenu();
 typedef struct _Flight
 {
     // "flight code, departure airport, destination airport, time of departure, time of destination and passenger capacity" records
-    unsigned short int flightCode; // between 1111 - 9999
-    char depAirport[AIRPORT_NAME_LENGTH]; // the maximum length of the airport names' strings are being controlled by "AIRPORT_NAME_LENGTH" macro
+    int flightCode;                        // between 1111 - 9999
+    char depAirport[AIRPORT_NAME_LENGTH];  // the maximum length of the airport names' strings are being controlled by "AIRPORT_NAME_LENGTH" macro
     char destAirport[AIRPORT_NAME_LENGTH]; // the maximum length of the airport names' strings are being controlled by "AIRPORT_NAME_LENGTH" macro
-    char timeOfDep[5]; // "HH.MM" format : time of departure
-    char timeOfDest[5]; // "HH.MM" format : time of destination
-    unsigned short int passengerCapacity; // passenger capacities of the planes (%hu)
+    float timeOfDep;                     // "HH.MM" format : time of departure
+    float timeOfDest;                    // "HH.MM" format : time of destination
+    int passengerCapacity;                 // passenger capacities of the planes
 } Flight;
 
 // a structuro of the bookings that passengers recorded
 typedef struct _Booking
 {
     char name[NAME_LENGTH]; // a string array to keep passengers names
-    unsigned long long personalId; // a variable to keep the passengers personal ID (%llu)
-    unsigned short int seatNumber; // a variable to keep seat data of booking (%hu)
-    unsigned short int bookingId; // a variable to keep assigned booking ID to passengers (%hu)
-    Flight flightInfo; // flight data of the bookings
+    int personalId;         // a variable to keep the passengers personal ID
+    int seatNumber;         // a variable to keep seat data of booking
+    int bookingId;          // a variable to keep assigned booking ID to passengers
+    Flight flightInfo;      // flight data of the bookings
 } Booking;
 
 int main()
@@ -71,53 +74,54 @@ int main()
         else
         {
             // pass to main menu if the adminstrator has signed up
-            switch(showMainMenu())
+            switch (showMainMenu())
             {
-                // adminstrator login option
+            // adminstrator login option
+            case 1:
+                // show options in admin menu
+                switch (adminLogin())
+                {
+                // adding a new flight
                 case 1:
-                    // show options in admin menu
-                    switch (showAdminMenu())
-                    {
-                        // adding a new flight
-                        case 1:
-                            break;
-                        // editing current flights
-                        case 2:
-                            break;
-                        // deleting flights
-                        case 3:
-                            break;
-                        // listing current flights
-                        case 4:
-                            break;
-                        // listing bookings   
-                        case 5:
-                            break;
-                        // changing password option
-                        case 6:
-                            createPassword();
-                            break;
-                        // returning to main menu option
-                        case 0:
-                            break;                      
-                        // default option for false inputs                                                                             
-                        default:
-                            printf("\n!!!!! An error occurred");
-                    }
+                    addFlight();
                     break;
-                // passenger menu option
+                // editing current flights
                 case 2:
-                    printf("passenger\n");
                     break;
-                // exiting from the program option
+                // deleting flights
+                case 3:
+                    break;
+                // listing current flights
+                case 4:
+                    break;
+                // listing bookings
+                case 5:
+                    break;
+                // changing password option
+                case 6:
+                    createPassword();
+                    break;
+                // returning to main menu option
                 case 0:
-                    printf("\nThe program has successfully closed.\nSEE YOU LATER!\n");
-
-                    getch(); // end of the program statement and provide that the program doesn't end without user permission
-                    return 0;
+                    break;
                 // default option for false inputs
                 default:
                     printf("\n!!!!! An error occurred");
+                }
+                break;
+            // passenger menu option
+            case 2:
+                printf("passenger\n");
+                break;
+            // exiting from the program option
+            case 0:
+                printf("\nThe program has successfully closed.\nSEE YOU LATER!\n");
+
+                getch(); // end of the program statement and provide that the program doesn't end without user permission
+                return 0;
+            // default option for false inputs
+            default:
+                printf("\n!!!!! An error occurred");
             }
         }
     }
@@ -129,7 +133,8 @@ int main()
 // functions
 
 // a function that makes menu switch designs
-void design(){
+void design()
+{
     printf("\n\n||||||||||||||||||||||||-------------||||||||||||||||||||||||\n");
     printf("|||||||||||||||||| << SATOSHI'S AIRPORT >> ||||||||||||||||||\n");
     printf("||||||||||||||||||||||||-------------||||||||||||||||||||||||\n\n");
@@ -139,7 +144,7 @@ void design(){
 int isAdminSignedUp()
 {
     // check the file database and find a adminstrator password
-    if (fopen("password.txt", "r") == NULL) // True IS TEMPORARY
+    if (fopen("password.txt", "r") == NULL)
     {
         // if there are not a password, return 0 and express that admin must be signed up
         return 0;
@@ -154,7 +159,7 @@ int isAdminSignedUp()
 // a function that provides the sign up for the first time and makes the password changes
 void createPassword()
 {
-    FILE *pwPtr; // pointer for password file database
+    FILE *pwPtr;                    // pointer for password file database
     char password[PASSWORD_LENGTH]; // a string that keeps new password
     // check if are there record on the password database
     //if there are not a password record, create password and provide sign up; else verify the old password and change it
@@ -211,24 +216,35 @@ int showMainMenu()
     return choice;
 }
 
-// a function to open adminstrator menu if user choose to be logined as adminstrator
-int showAdminMenu()
+// a function to provide access to the admin menu by password checking
+int adminLogin()
 {
     design();
 
-    FILE *pwPtr; // pointer for password file database
-    char password[2][PASSWORD_LENGTH]; // a string array that keeps passwords entered and signupped to match to login 
-    pwPtr = fopen("password.txt", "r");  // open file to read password from file database
-    fscanf(pwPtr, "%s", password[0]); // 0 indexed element is for getting and checking password from file database
+    FILE *pwPtr;                        // pointer for password file database
+    char password[2][PASSWORD_LENGTH];  // a string array that keeps passwords entered and signupped to match to login
+    pwPtr = fopen("password.txt", "r"); // open file to read password from file database
+    fscanf(pwPtr, "%s", password[0]);   // 0 indexed element is for getting and checking password from file database
 
     printf("Please enter the admin password: ");
     scanf("%s", password[1]); // 1 indexed element is for getting password from user and compare
 
     // comparing the passwords and if the input is correct, continue to the program
-    if(!strcmp(password[0], password[1]))
+    if (!strcmp(password[0], password[1]))
     {
-        design();
+        printf("\n");
+        return showAdminMenu(); // if the login comes true succesfully, direct to the admin menu
+    }
+    else
+    {
+        printf("\nThe password you entered is not correct!");
+        return 0; // if the password is not correct return to the main menu: the same return with 0 indexed option in the adminstrator menu
+    }
+}
 
+// a function to open adminstrator menu if the admin has loginned succesfully
+int showAdminMenu()
+{
         int choice;
         printf("1. Add a new flight\n");
         printf("2. Edit available flights\n");
@@ -241,11 +257,46 @@ int showAdminMenu()
         scanf("%d", &choice);
         return choice;
 
-    } else
-    {
-        printf("\nThe password you entered is not correct!");
-        return 0; // same return with 6th option in the adminstrator menu: return to the main menu
-    }
-    
+}
 
+// a function to add flights by adminstrator to file database
+void addFlight()
+{
+    design();
+
+    Flight newFlight; // creation of flight struct to make records
+    Flight *nfPtr;    // pointer to manage struct
+    nfPtr = &newFlight;
+
+    FILE *flPtr; // pointer for flights file database
+
+    // create file database if does not exist
+    if (fopen("flights.txt", "r") == NULL)
+    {
+        flPtr = fopen("flights.txt", "w");
+        fclose(flPtr);
+    }
+
+    // inputting required informations
+    nfPtr->flightCode = 1111;
+    printf("Please enter the departure airport: ");
+    scanf("%s", nfPtr->depAirport);
+    printf("\nPlease enter the destination airport: ");
+    scanf("%s", nfPtr->destAirport);
+    printf("\nPlease enter the time of departure: ");
+    scanf("%f", &nfPtr->timeOfDep);
+    printf("\nPlease enter the time of destination: ");
+    scanf("%f", &nfPtr->timeOfDest);
+    printf("\nPlease enter the capacity of the plane: ");
+    scanf("%d", &nfPtr->passengerCapacity);
+
+    // open the file, handle the inputted information and close
+    flPtr = fopen("flights.txt", "a");
+    fprintf(flPtr, "%d %s %s %.2f %.2f %d\n", nfPtr->flightCode, nfPtr->depAirport, nfPtr->destAirport, nfPtr->timeOfDep, nfPtr->timeOfDest, nfPtr->passengerCapacity);
+    printf("\n The flight has recorded succesfully...\n");
+    fclose(flPtr);
+
+    //return to the admin menu after the process
+    design();
+    showAdminMenu();
 }
